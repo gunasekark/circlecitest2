d3c34cbb6fd2d8b52f652580381440d1df7c5470
gunasekark <gunasekark@gmail.com>
2018-09-05 18:05:30 +0530

    Update abc.sh
    [skip ci]

--- abc.sh
+++ abc.sh
@@ -4,3 +4,5 @@ echo export var2=welcome >>$BASH_ENV
 echo export var3=welcome3 >>$BASH_ENV
 echo export var4c=welcome >> ~/.circlerc
 echo export var5c=welcome3 >> ~/.circlerc
+echo export var6=welcome >> ~/.bashrc
+echo export var7=welcome3 >> ~/.bashrc
